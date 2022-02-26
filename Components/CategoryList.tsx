7aeacb163ed7f81a96d3bc33cbeba5c42ab103c7
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2022-02-26 14:58:48 +0700

    TouchableOpacity, onPress, Alert

--- Components/CategoryList.tsx
+++ Components/CategoryList.tsx
@@ -1,41 +1,52 @@
 import * as React from 'react';
-import { Image, Text, View,StyleSheet } from 'react-native';
+import { Image, Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import Snowflake from "../assets/images/snowflakes.png";
-export interface  CategoryListProps {
-  category:{
-    id: number,
-    name:string
-  }
+export interface CategoryListProps {
+    category: {
+        id: number,
+        name: string
+    }
 }
 
-export default function CategoryList (props:  CategoryListProps) {
-  const {id,name} = props.category;
+export default function CategoryList(props: CategoryListProps) {
+    const { id, name } = props.category;
+    const handleTouch = () =>{
+        Alert.alert("Cảnh báo","Bạn là 1 con bóng long xiên xiên vô loz con đỉ mẻ m",[
+            {
+                text: 'Huỷ',
+                style: 'cancel'
+            },
+        ])
+    }
     return (
-    <>
-    <View style={style.container}>
-        <Text style={style.title}>{name}</Text>
-        <Image style={style.categoryImage} source={Snowflake}></Image>
-    </View>
-    </>
-  );
+        <>
+            <TouchableOpacity activeOpacity={0.5} onPress={handleTouch}>
+                <View style={style.container}>
+                    <Text style={style.title}>{name}</Text>
+                    <Image style={style.categoryImage} source={Snowflake}></Image>
+                </View>
+            </TouchableOpacity>
+
+        </>
+    );
 }
 const style = StyleSheet.create({
     categoryImage: {
-        height:200,
-        width:200
+        height: 200,
+        width: 200
     },
     container: {
         alignItems: 'center',
-        padding:4,
+        padding: 4,
         border: 1,
-        borderRadius:4,
+        borderRadius: 4,
         marginTop: 8,
         marginBottom: 8,
         backgroundColor: '#fff',
         shadowColor: '#000',
         shadowOpacity: 1,
         shadowRadius: 10,
-        shadowOffset: {width:10,height:10}
+        shadowOffset: { width: 10, height: 10 }
     },
     title: {
         fontSize: 30,
